c8cea80f58f883ca26cb0d7c5aa0292f74f66fba
LesmusTrompiz <javier.ortiz@edu.uah.es>
2022-09-06 21:21:40 +0200

    Serial improvements

--- ROS/src/serial_bridge/src/serial_bridge_client.py
+++ ROS/src/serial_bridge/src/serial_bridge_client.py
@@ -1,3 +1,4 @@
+from pickle import GLOBAL
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node   import Node
@@ -12,18 +13,29 @@ class OrderClient(Node):
         self._action_client = ActionClient(self, Order, 'serial_bridge_server')
 
     def send_goal(self, order):
-        self.get_logger().info('Sending Goal')
+        self.get_logger().info('Sending Goal')        
         goal_msg = order
         self._action_client.wait_for_server()
         return self._action_client.send_goal_async(goal_msg)
+
+    def user_loop(self):
+        new_order = int(input("New order? 0: To exit Else: Exit"))
+        while new_order != 0:
+            g     = Order.Goal()
+            g.id  = input("Order ID? ")
+            g.arg = int(input("Order ARG? "))
+            self.send_goal(g)
+            new_order = int(input("New order? 0: To exit Else: Exit"))
+        return
 
 
 def main(args=None):
     rclpy.init(args=args)
     action_client = OrderClient()
-    future = action_client.send_goal(Order.Goal())
-    rclpy.spin_until_future_complete(action_client, future)
-
+    action_client.user_loop()
+    #future = action_client.send_goal(Order.Goal())
+    #rclpy.spin_until_future_complete(action_client, future)
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
--- ROS/src/serial_bridge/src/serial_bridge_node.cpp
+++ ROS/src/serial_bridge/src/serial_bridge_node.cpp
@@ -19,29 +19,26 @@ class SerialBridgeNode : public rclcpp::Node
   public:
     using Order     = serial_bridge::action::Order;
     using GoalOrder = rclcpp_action::ServerGoalHandle<Order>;
-    SerialBridgeNode();
+    SerialBridgeNode(std::string port_name);
     ~SerialBridgeNode();
     rclcpp_action::GoalResponse     handle_goal(const rclcpp_action::GoalUUID & uuid,
                                             std::shared_ptr<const Order::Goal> goal);
     rclcpp_action::CancelResponse   handle_cancel(const std::shared_ptr<GoalOrder> goal_handle);
     void handle_accepted(const std::shared_ptr<GoalOrder> goal_handle);
 
+    //?api::Instance protocol;
+
   private:
     void timer_callback();
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     rclcpp_action::Server<serial_bridge::action::Order>::SharedPtr order_server;
-    size_t count_;
 };
 
-SerialBridgeNode::SerialBridgeNode()
-: Node("serial_bridge_node"), count_(0)
+SerialBridgeNode::SerialBridgeNode(std::string port_name)
+: Node("serial_bridge_node"), count_(0),vprotocol()
 {
   using namespace std::placeholders;
-
-  publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-  //timer_     = this->create_wall_timer(
-  //                    500ms, std::bind(&SerialBridgeNode::timer_callback, this));
   
   order_server =  rclcpp_action::create_server<serial_bridge::action::Order>(
     this,
@@ -49,6 +46,9 @@ SerialBridgeNode::SerialBridgeNode()
     std::bind(&SerialBridgeNode::handle_goal,     this, _1, _2),
     std::bind(&SerialBridgeNode::handle_cancel,   this, _1),
     std::bind(&SerialBridgeNode::handle_accepted, this, _1));
+    get_parameter("port_name", port_name);
+
+    //?protocol.hook_serial_port(port);
 }
 
 SerialBridgeNode::~SerialBridgeNode(){}
@@ -84,14 +84,12 @@ rclcpp_action::CancelResponse  SerialBridgeNode::handle_cancel(
 void SerialBridgeNode::handle_accepted(const 
   std::shared_ptr<GoalOrder> goal_handle)
 {
-
   const auto goal = goal_handle->get_goal();
-  const auto id   = goal->id;
-  const auto arg  = goal->arg;
-  const auto time_stamp = goal->time_stamp;
-  RCLCPP_INFO(this->get_logger(), "Handling order: Id %u, Arg %d, TimeStamp %u", id, arg, time_stamp );
-  auto result = std::make_shared<Order::Result>();
+  RCLCPP_INFO(this->get_logger(), "Handling order: Id %s, Arg %d", goal->id.c_str(), goal->arg);
+  auto result   = std::make_shared<Order::Result>();
+  //result.return = rmi.invoque(goal->id,goal->arg);
   goal_handle->succeed(result);
+  return;
 }
 
 
@@ -99,7 +97,7 @@ void SerialBridgeNode::handle_accepted(const
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<SerialBridgeNode>());
+  rclcpp::spin(std::make_shared<SerialBridgeNode>("hola"));
   rclcpp::shutdown();
   return 0;
 }
