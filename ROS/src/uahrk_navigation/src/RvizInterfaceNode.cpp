8a453282197e07780669c9ba8c905e08140e6cce
LesmusTrompiz <javier.ortiz@edu.uah.es>
2022-10-30 14:45:54 +0100

    Added to RvizInterface some callback fns to log the result of the action result

--- ROS/src/uahrk_navigation/src/RvizInterfaceNode.cpp
+++ ROS/src/uahrk_navigation/src/RvizInterfaceNode.cpp
@@ -15,7 +15,7 @@
 using namespace std::chrono_literals;
 using GoToPose  = uahrk_navigation_msgs::action::GoToPose;
 
-//using GoalHandleGoToPose = rclcpp_action::ServerGoalHandle<GoToPose>;
+using GoalHandleGoToPose = rclcpp_action::ClientGoalHandle<GoToPose>;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
@@ -34,6 +34,7 @@ class RvizInterfaceNode : public rclcpp::Node
                   "goal_pose", 10,
                 std::bind(&RvizInterfaceNode::goal_pose, this, std::placeholders::_1));
       client_ptr_ = rclcpp_action::create_client<GoToPose>(this, "move_server");
+
     }
 
   private:
@@ -49,14 +50,45 @@ class RvizInterfaceNode : public rclcpp::Node
 
     void goal_pose(geometry_msgs::msg::PoseStamped::UniquePtr rviz_pose)
     {
-      //geometry_msgs::msg::Pose p;
-      //p.position    = rviz_pose->pose.position;
-      //p.orientation = rviz_pose->pose.orientation;
+      using namespace std::placeholders;
       RCLCPP_INFO(this->get_logger(), "Sending goal: '%f'", rviz_pose->pose.position.x);
       auto goal_msg = GoToPose::Goal();
       goal_msg.pose = *rviz_pose;
-      client_ptr_->async_send_goal(goal_msg);
+
+      auto send_goal_options = rclcpp_action::Client<GoToPose>::SendGoalOptions();
+      send_goal_options.goal_response_callback = std::bind(&RvizInterfaceNode::goal_response_callback, this, _1);
+      send_goal_options.result_callback = std::bind(&RvizInterfaceNode::result_callback, this, _1);
+      client_ptr_->async_send_goal(goal_msg, send_goal_options);
       return;
+    }
+
+    void goal_response_callback(std::shared_future<GoalHandleGoToPose::SharedPtr> future)
+    {
+      auto goal_handle = future.get();
+      if (!goal_handle) {
+        RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+      } else {
+        RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+      }
+    }
+    void result_callback(const GoalHandleGoToPose::WrappedResult & result)
+    {
+      switch (result.code) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+          break;
+        case rclcpp_action::ResultCode::ABORTED:
+          RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+          return;
+        case rclcpp_action::ResultCode::CANCELED:
+          RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+          return;
+        default:
+          RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+          return;
+      }
+      std::stringstream ss;
+      ss << "Result received: " << result.result->result.data;
+      RCLCPP_INFO(this->get_logger(), ss.str().c_str());
     }
 
     rclcpp_action::Client<GoToPose>::SharedPtr client_ptr_;
