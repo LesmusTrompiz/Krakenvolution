1693aafadfbcb0ac53bc05de83be8b297db58c23
LesmusTrompiz <javier.ortiz@edu.uah.es>
2022-10-30 14:04:18 +0100

    Rviz Interface node now request move orders when a goal is selected in rviz

--- ROS/src/uahrk_navigation/src/RvizInterfaceNode.cpp
+++ ROS/src/uahrk_navigation/src/RvizInterfaceNode.cpp
@@ -4,10 +4,18 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 #include "geometry_msgs/msg/pose.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "uahrk_navigation_msgs/action/go_to_pose.hpp"
+
+
 
 using namespace std::chrono_literals;
+using GoToPose  = uahrk_navigation_msgs::action::GoToPose;
+
+//using GoalHandleGoToPose = rclcpp_action::ServerGoalHandle<GoToPose>;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
@@ -16,13 +24,16 @@ class RvizInterfaceNode : public rclcpp::Node
 {
   public:
     RvizInterfaceNode()
-    : Node("minimal_publisher")
+    : Node("rviz_interface")
     {
       publisher_    = this->create_publisher<geometry_msgs::msg::Pose>("robot_odom", 10);
       subscription_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
                   "initialpose", 10,
                 std::bind(&RvizInterfaceNode::pose_estimate_callback, this, std::placeholders::_1));
-  
+      subscription2_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+                  "goal_pose", 10,
+                std::bind(&RvizInterfaceNode::goal_pose, this, std::placeholders::_1));
+      client_ptr_ = rclcpp_action::create_client<GoToPose>(this, "move_server");
     }
 
   private:
@@ -35,10 +46,24 @@ class RvizInterfaceNode : public rclcpp::Node
       RCLCPP_INFO(this->get_logger(), "Publishing: '%f'", p.position.x);
       return;
     }
+
+    void goal_pose(geometry_msgs::msg::PoseStamped::UniquePtr rviz_pose)
+    {
+      //geometry_msgs::msg::Pose p;
+      //p.position    = rviz_pose->pose.position;
+      //p.orientation = rviz_pose->pose.orientation;
+      RCLCPP_INFO(this->get_logger(), "Sending goal: '%f'", rviz_pose->pose.position.x);
+      auto goal_msg = GoToPose::Goal();
+      goal_msg.pose = *rviz_pose;
+      client_ptr_->async_send_goal(goal_msg);
+      return;
+    }
+
+    rclcpp_action::Client<GoToPose>::SharedPtr client_ptr_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr publisher_;
     rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr subscription_;
-
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr subscription2_;
 };
 
 int main(int argc, char * argv[])
