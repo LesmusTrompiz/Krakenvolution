4ad18495ea80a44e5105e1f0078c30a9a03b954a
LesmusTrompiz <javier.ortiz@edu.uah.es>
2022-11-13 12:35:55 +0100

    Added an small state machine to clarify MoveToPose behaviour

--- ROS/src/uahrk_navigation/include/uahrk_navigation/MoveToPoseNode.hpp
+++ ROS/src/uahrk_navigation/include/uahrk_navigation/MoveToPoseNode.hpp
@@ -2,16 +2,17 @@
 #include <functional>
 #include <memory>
 #include <string>
-#include "uahrk_navigation/point2d.hpp"
+#include "uahrk_navigation/move_utils.hpp"
+
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "serial_bridge_actions/action/order.hpp"
 #include "uahrk_navigation_msgs/action/go_to_pose.hpp"
-
-//using go_to_pose = uahrk_navigation_msgs::action::GoToPose;
-//using Fibonacci = action_tutorials_interfaces::action::Fibonacci;
+#include "tf2/exceptions.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
 
 
 using Order     = serial_bridge_actions::action::Order;
@@ -20,17 +21,24 @@ using RequestHandleOrder = rclcpp_action::ClientGoalHandle<Order>;
 using GoToPose  = uahrk_navigation_msgs::action::GoToPose;
 using GoalHandleGoToPose = rclcpp_action::ServerGoalHandle<GoToPose>;
 
+enum ControlState {IDLE, EXECUTING, NEXT_MOVE};
+
 class MoveToPoseNode : public rclcpp::Node
 {
-  
   public:
     MoveToPoseNode();
   private:
     rclcpp_action::Server<GoToPose>::SharedPtr go_to_pose_server;
     rclcpp_action::Client<Order>::SharedPtr    order_client;
-    
+    tf2::BufferCore tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    ControlState state;        
+    rclcpp_action::ResultCode order_result;
+//    std::shared_ptr<const std::shared_ptr<GoalHandleGoToPose>> actual_handle;
+    std::shared_ptr<GoalHandleGoToPose> actual_handle;
 
-    
+
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID & uuid,
         std::shared_ptr<const GoToPose::Goal> goal);
@@ -41,28 +49,11 @@ class MoveToPoseNode : public rclcpp::Node
     void handle_accepted(
       const std::shared_ptr<GoalHandleGoToPose> goal_handle);
     
+
+    void control_cycle();
     void execute(const std::shared_ptr<GoalHandleGoToPose> goal_handle);
     void result_callback(const RequestHandleOrder::WrappedResult & result);
-    void send_order();
-
+    void send_order(std::string id, int16_t arg);
+    Pose2d get_robot_pose();
 };
-
-int spin_to_goal(float robot_alfa, float goal_alfa);
-
-inline int advance_to_goal(const Point2d &robot, const Point2d &goal){
-  /**
-   * @brief This function calculates the advance
-   * needed by the robot to accomplish the xy
-   * goal.
-   * 
-   * @param robot Actual xy position of the robot
-   * @param goal Goal xy position of the goal
-   */
-  
-    // Return the eculidean distance
-    return sqrt(((robot.x - goal.x) * (robot.x - goal.x)) + ((robot.y - goal.y) * (robot.y - goal.y)));
-}
-
-
-
 
--- ROS/src/uahrk_navigation/src/uahrk_navigation/MoveToPoseNode.cpp
+++ ROS/src/uahrk_navigation/src/uahrk_navigation/MoveToPoseNode.cpp
@@ -1,16 +1,13 @@
 #include "uahrk_navigation/MoveToPoseNode.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
-
 using namespace std::chrono_literals;
 
-extern "C"{
-    #include <cassert>
-    #include <math.h>
-}
 
 MoveToPoseNode::MoveToPoseNode()
-  : Node("move_to_pose_node")
+  : Node("move_to_pose_node"),
+    tf_buffer_(),
+    tf_listener_(tf_buffer_)
   {
     //RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     using namespace std::placeholders;
@@ -23,25 +20,107 @@ MoveToPoseNode::MoveToPoseNode()
       "move_server",
       std::bind(&MoveToPoseNode::handle_goal, this, _1, _2),
       std::bind(&MoveToPoseNode::handle_cancel, this, _1),
-      std::bind(&MoveToPoseNode::handle_accepted, this, _1));    
+      std::bind(&MoveToPoseNode::handle_accepted, this, _1));  
+
+    // Create a timer that will call the control_cycle
+    // every 100ms
+    timer_ = create_wall_timer(
+      100ms, std::bind(&MoveToPoseNode::control_cycle, this));  
   }
+
+void MoveToPoseNode::control_cycle(){
+  auto result = std::make_shared<GoToPose::Result>();
+  float dist_precision  = 0.5;
+  float angle_precision = 10;
+
+  switch (state)
+  {
+    case IDLE:
+      RCLCPP_INFO(this->get_logger(), "IDLE STATE");
+      if (actual_handle != nullptr && actual_handle->is_active()){
+        state = NEXT_MOVE;
+      }
+    break;
+    case NEXT_MOVE:
+      try {
+        RCLCPP_INFO(this->get_logger(), "NEXT MOVE STATE");
+        auto robot_pose = get_robot_pose();
+        Pose2d goal_pose{actual_handle->get_goal()->pose.pose};
+        if (robot_in_goal(robot_pose, goal_pose, dist_precision, angle_precision)){
+          actual_handle->succeed(result);
+          state = IDLE;
+        }
+        else{
+          auto move = calculate_move(robot_pose, goal_pose, dist_precision, angle_precision);
+          send_order(std::get<0>(move),std::get<1>(move));
+          state = EXECUTING;
+        }
+      }
+      catch (tf2::TransformException & ex) {
+        RCLCPP_WARN(get_logger(), "Obstacle transform not found: %s", ex.what());
+        actual_handle->abort(result);
+        state = IDLE;
+        return;
+      }
+      catch (const std::exception &exc)
+      {
+          // catch anything thrown within try block that derives from std::exception
+          std::cerr << exc.what();
+          RCLCPP_ERROR(get_logger(), "Unknown Exception not found: %s", exc.what());
+          actual_handle->abort(result);
+          state = IDLE;
+          return;
+      }
+    break;
+    case EXECUTING:
+      // Check out if the order to serial
+      // Node has finished
+      RCLCPP_INFO(this->get_logger(), "EXECUTING");
+      switch (order_result) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+          state = NEXT_MOVE;
+          break;
+        case rclcpp_action::ResultCode::ABORTED:
+          actual_handle->abort(result);
+          state = IDLE;
+          RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+        case rclcpp_action::ResultCode::CANCELED:
+          RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+          actual_handle->abort(result);
+          state = IDLE;
+          break;
+        default:
+          break;
+      }
+    break;
+  default:
+    break;
+  }
+}
+
 
 /*
   Client Functions:
 */
 
-void MoveToPoseNode::send_order()
+void MoveToPoseNode::send_order(std::string id, int16_t arg)
 {
   auto send_goal_options = rclcpp_action::Client<Order>::SendGoalOptions();
-  send_goal_options.result_callback = std::bind(&MoveToPoseNode::result_callback, this, std::placeholders::_1);
   auto goal_msg = Order::Goal();
-  goal_msg.id  = "Prueba";
-  goal_msg.arg = 1;
+  order_result = rclcpp_action::ResultCode::UNKNOWN;
+
+  send_goal_options.result_callback = std::bind(&MoveToPoseNode::result_callback, this, std::placeholders::_1);
+
+  goal_msg.id  = id;
+  goal_msg.arg = arg;
+  RCLCPP_INFO(this->get_logger(), "Sending msg %s %i", id.c_str(), arg);
+
   order_client->async_send_goal(goal_msg, send_goal_options);
 }
 
 void MoveToPoseNode::result_callback(const RequestHandleOrder::WrappedResult & result)
 {
+  order_result = result.code;
   switch (result.code) {
     case rclcpp_action::ResultCode::SUCCEEDED:
       break;
@@ -59,6 +138,9 @@ void MoveToPoseNode::result_callback(const RequestHandleOrder::WrappedResult & r
   ss << "Result received: " << result.result->ret;
   RCLCPP_INFO(this->get_logger(), ss.str().c_str());
 }
+
+
+
 
 /*
   SERVER FUNCS:
@@ -92,51 +174,71 @@ void MoveToPoseNode::handle_accepted(const std::shared_ptr<GoalHandleGoToPose> g
   using namespace std::placeholders;
 
   // this needs to return quickly to avoid blocking the executor, so spin up a new thread
-  std::thread{std::bind(&MoveToPoseNode::execute, this, _1), goal_handle}.detach();
+  //std::thread{std::bind(&MoveToPoseNode::execute, this, _1), goal_handle}.detach();
+  actual_handle = goal_handle;
 }
 
+Pose2d MoveToPoseNode::get_robot_pose(){
+    auto robot_pose = tf_buffer_.lookupTransform(
+      "map", "robot", tf2::TimePointZero);
+    return {robot_pose};
+}
 
 void MoveToPoseNode::execute(const std::shared_ptr<GoalHandleGoToPose> goal_handle)
 {
   RCLCPP_INFO(this->get_logger(), "Executing goal");
+  float dist_precision  = 0.05;
+  float angle_precision = 0.05;
+  int dist = 0;
+  int spin = 0;
+
   rclcpp::Rate loop_rate(1);
   const auto goal = goal_handle->get_goal();
-  send_order();
   auto result = std::make_shared<GoToPose::Result>();
-  result->result.data = true;
+  Pose2d g{goal->pose.pose};
+  Pose2d robot_pose;
+
+  while(true){
+    try {
+      robot_pose = get_robot_pose();
+    }
+    catch (tf2::TransformException & ex) {
+      RCLCPP_WARN(get_logger(), "Obstacle transform not found: %s", ex.what());
+      goal_handle->abort(result);
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "robot_pose %f %f %f", robot_pose.x, robot_pose.y, robot_pose.a);
+    RCLCPP_INFO(this->get_logger(), "goal_pose %f %f %f", g.x, g.y, g.a);
+
+    dist = advance_to_goal(robot_pose, g);
+    spin = spin_to_goal(robot_pose.a, g.a);
+    if(dist <= dist_precision){
+      if(spin <= angle_precision){
+        result->result.data = true;
+        goal_handle->succeed(result);
+        return;
+      }
+      else{
+        send_order("spin",spin);
+      }
+    }
+    else{
+      spin = spin_to_wp(robot_pose, g);
+      if ((spin < 5) && (spin > -5)){
+        send_order("advance",dist * 1000);
+      }
+      else{
+        send_order("spin",spin);
+      }
+    }
+    loop_rate.sleep();
+  }
   RCLCPP_INFO(this->get_logger(), "Goal executed");
-  goal_handle->succeed(result);
+  return;
 }
 
 
 
-
-int spin_to_goal(const float robot_alfa, const float goal_alfa){
-  /**
-   * @brief This function calculates the spin
-   * needed by the robot to accomplish the 
-   * angle goal.
-   * 
-   * @param robot_alfa Actual rotation of the robot
-   * in degrees, the expected range is from 
-   * -180 to 180 (ROS STANDARD).
-   * @param goal_alfa Goal rotation in degrees 
-   * the expected range is from  -180 to 180
-   * (ROS STANDARD).
-   */
-  
-  // Check that arguments are in the expected range
-  assert(robot_alfa <=  180);
-  assert(robot_alfa >= -180);
-  assert(goal_alfa  <=  180);
-  assert(goal_alfa  >= -180);
-
-
-  int spin = goal_alfa - robot_alfa;
-  if      (spin >  180) return spin - 360;
-  else if (spin < -180) return spin + 360;
-  else                  return spin;
-}
 
 
 
