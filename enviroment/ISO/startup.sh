3855fb9cfcb65e28ab22f5d1c84ce23b033bf03d
LesmusTrompiz <javier.ortiz@edu.uah.es>
2023-04-03 20:08:32 +0200

    ros ws path changed

--- enviroment/ISO/startup.sh
+++ enviroment/ISO/startup.sh
@@ -2,7 +2,7 @@
 
 # Load ROS2
 source /opt/ros/foxy/setup.bash
-source  /home/krakenvolution/ROS/install
+source /home/krakenvolution/Krakenvolution/ROS/install/setup.bash
 
 # Load nodes
 ros2 launch uahrk_bring_up roboot.py
