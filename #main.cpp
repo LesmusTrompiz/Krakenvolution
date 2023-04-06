3e6c676ddde889f69417e8ff422e6b00f67ca438
navil2000 <navil.abdeselam@edu.uah.es>
2023-04-06 16:24:14 +0200

    Parejitas control added to projecto project

---
+++ #main.cpp
@@ -0,0 +1,180 @@
+// #define debug_mode
+#include <RobotServos.hpp>
+#include <Adafruit_PWMServoDriver.h>
+#include <Arduino.h>
+#include <eurouart.hpp>
+#include <protocol.hpp>
+#include <motion_controller.hpp>
+#include <timer_&_pwm.hpp>
+#include <pines_&_constexpr.hpp>
+
+/* Iniciamos el protocolo */
+uahruart::parser::Protocol protocol;
+
+/* Servos */
+Adafruit_PWMServoDriver ServoHandlerMaster = Adafruit_PWMServoDriver(0x40);
+auto servo_disparador = RobotServo(pwm_disparador, ServoHandlerMaster);
+//auto servo_...
+[1;5B[1;5B[1;5B[1;5B
+/* Definición completa de la mecánica del robot */
+
+Param_mecanicos mecanica_tactico(
+ tactico_acel,
+ tactico_decel,
+ tactico_reductora,
+ tactico_vel_eje_max,
+ tactico_vel_max,
+ vel_giro_tactico,
+ vel_freno_tactico,
+ tactico_pulsos_por_rev,
+ tactico_L,
+ tactico_diametro
+);
+
+Odom odom_tactico;
+
+Motores motores_tactico(
+	tactico_vel_max,
+	D_EN,
+	D_DIR,
+	PWM6_SetDuty,
+	I_EN,
+	I_DIR,
+	PWM5_SetDuty
+);
+
+PerfilVelocidad perfilador_tactico(
+	ajuste_dist_recta_tactico,
+	ajuste_dist_giro_tactico,
+	mecanica_tactico
+);
+
+motion_controller controlador_tactico(
+	mecanica_tactico,
+	odom_tactico,
+	motores_tactico,
+	perfilador_tactico
+);
+
+/* Interrupciones para odometría */
+void int_odom_derecha()
+{
+	// controlador_tactico.odom.cuentas_derecha_total++;
+
+	if(digitalReadDirect(Enc_D))
+		controlador_tactico.odom.cuentas_derecha--;
+	else
+		controlador_tactico.odom.cuentas_derecha++;
+}
+void int_odom_izquierda()
+{
+	// controlador_tactico.odom.cuentas_izquierda_total++;
+
+	if(digitalReadDirect(Enc_I))
+		controlador_tactico.odom.cuentas_izquierda++;
+	else
+		controlador_tactico.odom.cuentas_izquierda--;
+}
+
+/* Registro de métodos en el protocolo de comunicación */
+void setup_serial_protocol() 
+{
+    protocol.on_write([](const char* msg) 
+		{
+        Serial.println(msg);
+    });
+
+    // Register methods
+    protocol.register_method("traction", "turn", [](int32_t arg)
+		{
+        controlador_tactico.ref_ang = static_cast<float>(arg);
+        controlador_tactico.prev_move_calculus(0);
+    });
+
+    protocol.register_method("traction", "advance", [](int32_t arg)
+		{
+        controlador_tactico.ref_distancia = static_cast<float>(arg);
+        controlador_tactico.prev_move_calculus(1);
+    });
+
+    protocol.register_method("admin", "reset", [](int32_t arg) 
+		{
+        rstc_start_software_reset(RSTC);
+    });
+
+    protocol.register_method("actuadores", "servo_disparador", [](int32_t arg) 
+		{
+        servo_disparador.set_angle(arg);
+    });
+
+    on_finished([]() 
+		{
+        uahruart::primitives::Bool test = true;
+        protocol.send(test);
+    });
+}
+
+/* Configuraciones y bucle de control */
+void setup() 
+{
+	// Adafruit driver conf
+	ServoHandlerMaster.begin();
+	ServoHandlerMaster.setPWMFreq(50);
+	// Serial conf
+	Serial.begin(115200);
+  setup_serial_protocol();
+	// Pines para el motor
+	pinMode(D_EN, OUTPUT);
+	pinMode(I_EN, OUTPUT);
+	pinMode(D_DIR, OUTPUT);
+	pinMode(I_DIR, OUTPUT);
+	// Int conf
+	pinMode(PCInt_D,INPUT);
+	pinMode(Enc_D,INPUT);
+	pinMode(PCInt_I,INPUT);	
+	pinMode(Enc_I,INPUT);
+
+	attachInterrupt(digitalPinToInterrupt(PCInt_I), int_odom_izquierda, RISING);
+	attachInterrupt(digitalPinToInterrupt(PCInt_D), int_odom_derecha, RISING); 
+
+	// PWMs and Timer (50Hz)
+	config_pwms();
+	config_timer(TC0, 2, TC2_IRQn, 50);
+
+	// Reset de odometría para empezar
+	controlador_tactico.odom.reset_odom();
+
+	// Habilitar las controladoras
+	controlador_tactico.motores.encender_motores();
+
+	// Check...
+}
+
+void loop() 
+{
+	serialEvent();
+}
+
+/* Bucle de control -> TC2 Handler*/
+void TC2_Handler(void)
+{
+  TC_GetStatus(TC0, 2);
+
+	#ifdef debug_mode	
+		static int debugger = 0;
+		if(debugger >= 1*(50))
+		{
+			Serial.print("CR: ");Serial.println(controlador_tactico.odom.cuentas_derecha);
+			Serial.print("CL: ");Serial.println(controlador_tactico.odom.cuentas_izquierda);
+
+			Serial.print("X: ");Serial.println(controlador_tactico.odom.pose_actual.x);
+			Serial.print("Y: ");Serial.println(controlador_tactico.odom.pose_actual.y);
+			Serial.print("O: ");Serial.println(controlador_tactico.odom.pose_actual.alfa);
+			debugger = 0;
+		}
+		debugger++;
+	#endif
+
+	// Ejecutar control
+  controlador_tactico.move_control();
+}
---
+++ Control/Control Parejitas/src/RobotServos.cpp
@@ -0,0 +1,69 @@
+#include "RobotServos.hpp"
+
+RobotServo::RobotServo(const uint8_t _ID_AnalogRead, const uint8_t _PWM_ID, Adafruit_PWMServoDriver _ServoHandler)
+: ID_AnalogRead(_ID_AnalogRead), PWM_ID(_PWM_ID), ServoHandler(_ServoHandler)
+{
+  // Lectura analógica
+  pinMode(ID_AnalogRead, INPUT);
+}
+
+RobotServo::RobotServo(const uint8_t _PWM_ID, Adafruit_PWMServoDriver _ServoHandler)
+: ID_AnalogRead(-1), PWM_ID(_PWM_ID), ServoHandler(_ServoHandler)
+{
+  // Sin lectura analógica.
+}
+
+float RobotServo::read_angle()
+{
+  /**
+   * @brief Función para lectura del ángulo actual 
+  */
+	if(ID_AnalogRead != -1)
+	{
+		int n_reads = 10;
+		// 1. Hacemos n_reads lecturas analógicas.
+		int read_value[n_reads];
+		for(int i=0; i<n_reads; i++){read_value[i] = analogRead(ID_AnalogRead);}
+		// 2. Media de las medidas tomadas.
+		float final_r = 0;
+		for(int i=0; i<n_reads; i++){final_r += read_value[i];}
+		final_r = final_r/n_reads;
+		// 3. Transformamos la lectura a ángulo -> Transformación dada por fabricante de los servos.
+		return final_r*m_read + n_read;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+void RobotServo::set_angle(float angle)
+{
+  // 1. Ticks nominal
+  float nominal_tick_value = m*angle + n;
+  // 2. Mandar la señal y esperar a que se estabilice la posiciónd el servo
+  ServoHandler.setPWM(PWM_ID, 0, (int)(nominal_tick_value));
+}
+
+void RobotServo::calibration(float angle_init, float angle_final)
+{
+	if(ID_AnalogRead != -1)
+	{
+		float tick_init   = m*angle_init + n;
+		float tick_final  = m*angle_final + n;
+
+		// 1. Valor inicial de la recta.
+		set_angle(angle_init);
+		delay(1000);
+		float angle_init_real = read_angle();
+		// 2. Valor final de la recta.
+		set_angle(angle_final);
+		delay(1000);
+		float angle_final_real = read_angle();
+		// 3. Definición de la recta.
+		m = (tick_final-tick_init)/(angle_final_real-angle_init_real);
+		n = ((tick_final-m*angle_final_real)+(tick_init-m*angle_init_real))/2;  
+	}
+	else
+		return;
+}
