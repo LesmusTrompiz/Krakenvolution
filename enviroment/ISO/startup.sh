2d95c9a6fce591b0fae675f02007ee46845361d1
LesmusTrompiz <javier.ortiz@edu.uah.es>
2023-04-10 02:02:19 +0200

    Startup sh script

--- enviroment/ISO/startup.sh
+++ enviroment/ISO/startup.sh
@@ -5,7 +5,7 @@ export HOME=/home/root/
 
 # Load ROS2
 source /opt/ros/foxy/setup.bash
-source /home/root/Krakenvolution/ROS/install/setup.bash
+source /root/Krakenvolution/ROS/install/setup.bash
 
 # Load nodes
 ros2 launch uahrk_bring_up roboot.py
